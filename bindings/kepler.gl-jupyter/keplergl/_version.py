4eb8f005d05c76ec7c6c14baa2fb4cee5076cda7
Sourcery AI <>
2023-05-06 18:33:18 +0000

    'Refactored by Sourcery'

--- bindings/kepler.gl-jupyter/keplergl/_version.py
+++ bindings/kepler.gl-jupyter/keplergl/_version.py
@@ -2,8 +2,7 @@ version_info = (0, 3, 0, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
-__version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
-  '' if version_info[3]=='final' else '-' + _specifier_[version_info[3]]+str(version_info[4]))
+__version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}{'' if version_info[3] == 'final' else f'-{_specifier_[version_info[3]]}{str(version_info[4])}'}"
 # The version of the attribute spec that this package
 # implements. This is the value used in
 # _model_module_version/_view_module_version.
